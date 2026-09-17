7bd8942fc713027c07676a56c748eee157a431a5
Mohammed Iqbal <mohiqbal2rrr@gmail.com>
2026-09-18 00:04:48 +0530

    first commit

---
+++ src/main/java/Controller/UserController.java
@@ -0,0 +1,28 @@
+package Controller;
+
+
+import Model.User;
+import Security.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User Register(@RequestBody User user){
+        return userService.register();
+    }
+
+    @GetMapping("/login")
+
+    public User login(@RequestBody User user){
+        return userService.login();
+    }
+}
---
+++ src/main/java/Model/User.java
@@ -0,0 +1,27 @@
+package Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
